c87d340b4ae1339ff10b86a3eb6bd6b6c3cea492
Igor Polevoy <igor@expresspigeon.com>
2014-12-05 15:16:35 -0600

    copy property files to output

--- build.sh
+++ build.sh
@@ -7,6 +7,12 @@ cp -r src/content/images/ output/blog_images
 
 for file in `find src/content/posts/ -name *.md`
  do
+   export file_name=`echo $file | tr '.' ' ' | awk '{print $1}'`
+   cp $file_name.properties output
+done
+
+for file in `find src/content/posts/ -name *.md`
+ do
    export file_name=`echo $file | tr '/' ' ' | awk '{print $5}'`
    pandoc -f markdown -t html   $file -o output/$file_name.html
 done
